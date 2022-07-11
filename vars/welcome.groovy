cb1e6ef2f00118a74f292c578cf2947d7a9f506f
Jitendra <59396681+jitendra-github-lab@users.noreply.github.com>
2022-07-11 18:36:27 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def cloneGitRepo(String url) {
+def call(String url) {
   sh "git clone ${url}"
   commit_id = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
   return commit_id
