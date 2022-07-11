93d6778aa294be298fd26dd516d18e2d9c6c5273
Jitendra <59396681+jitendra-github-lab@users.noreply.github.com>
2022-07-11 18:46:29 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -3,7 +3,8 @@
 def call(String url) {
   sh "git clone ${url}"
   sh "ls -ltr -a"
-  sh "cd docker-practical"
-  commit_id = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
+  dir('docker-practical'){
+    commit_id = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
+  }
   return commit_id
 }
